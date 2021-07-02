5c61a5ea2660c96589f859fa324cdc5c25ddbcd2
Erick <elias25533@gmail.com>
2021-07-02 12:02:09 -0400

    Update Unit tests

--- Docker--Nodejs---SQLServer/crud-nodejs-mysql-master/src/test/routesTest.js
+++ Docker--Nodejs---SQLServer/crud-nodejs-mysql-master/src/test/routesTest.js
@@ -4,8 +4,8 @@ const app = require('../app')
 chai.use(chaiHttp)
 
 describe('Pruebas del backend', () => {
-    it('Obteniendo reportes',(done) => {
-        chai.request(app).get("/reporte").end((err, res) => {
+    it('Obteniendo categorias',(done) => {
+        chai.request(app).get("/categoria").end((err, res) => {
             if(err) done(err)
             chai.expect(res.body).to.be.an('array')
             done()
@@ -23,10 +23,42 @@ describe('Pruebas del backend', () => {
         })
     })
 
-    it('Obteniendo reporte específico',(done) => {
-        chai.request(app).get("/reporte/10000").end((err, res) => {
+    it('Obteniendo usuarios',(done) => {
+        chai.request(app).get("/usuario").end((err, res) => {
+            if(err) done(err)
+            chai.expect(res.body).to.be.an('array')
+            done()
+        })
+    })
+
+    it('Obteniendo productos',(done) => {
+        chai.request(app).get("/producto").end((err, res) => {
+            if(err) done(err)
+            chai.expect(res.body).to.be.an('array')
+            done()
+        })
+    })
+
+    it('carro de compras agregado',(done) => {
+        chai.request(app).get("/carroProducto").end((err, res) => {
+            if(err) done(err)
+            chai.expect(res.body).to.be.an('array')
+            done()
+        })
+    })
+
+    it('Update de producto inexistente',(done) => {
+        chai.request(app).post("/updateProducto/100000").end((err, res) => {
             if(err) done(err)
             chai.expect(res.body).to.have.length.above(0);
+            done()
+        })
+    })
+
+    it('Eliminar productos',(done) => {
+        chai.request(app).delete("/producto").end((err, res) => {
+            if(err) done(err)
+            chai.expect(res.body).to.be.an('array')
             done()
         })
     })
